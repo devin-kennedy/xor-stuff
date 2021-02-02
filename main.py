f0f3ce03c850eb35bde2e0d857003202ef4b8883
Llaaamaguy <67341568+Llaaamaguy@users.noreply.github.com>
2021-02-02 15:07:53 -0500

    Cipher

---
+++ main.py
@@ -0,0 +1,36 @@
+from base64 import b64encode, b64decode
+
+mode = input("Mode? E for encrypt or D for decrypt\n")
+if mode == "E":
+    message = input("What is your message\n")
+key = input("What is key\n")
+key = int(key)
+
+def single_byte_xor(message, key, encrypt=True):
+    if encrypt:
+        bs = bytearray(message, encoding='ASCII')
+    else:
+        bs = bytearray(b64decode(message))
+
+    ba = bytearray()
+    for b in bs:
+        ba.append(b^key)
+
+    if encrypt:
+        encoded = b64encode(ba)
+        return "B64 encoded: " + str(encoded)
+    else:
+        decoded = ba.decode("ASCII")
+        return "B64 decoded: " + str(decoded)
+    
+
+
+if mode == "E":
+    print(single_byte_xor(message, key, encrypt=True))
+elif mode == "D":
+    with open("cipher.txt", "r") as f:
+        message = f.readline()
+        f.close()
+    print(single_byte_xor(message, key, encrypt=False))
+else:
+    print("Invalid mode entered")
